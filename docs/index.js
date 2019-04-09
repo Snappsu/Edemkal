a04f51cd3de317342500acb6286e370a2b36de9c
Snapps <snapples64@gmail.com>
2019-04-09 09:04:14 -0700

    Changed website

---
+++ docs/index.js
@@ -0,0 +1,26 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+
+    
+    var time = h + ":" + m + " " + session;
+    document.getElementById("time-text").innerHTML = time;    
+    setTimeout(showTime, 1000);
+}
+
+showTime();
--- docs/index.md
+++
@@ -1,5 +0,0 @@
-
-# What's here?
-## Edemkal Safari 
-* [The Game](https://snappsu.github.io/Edemkal/edemkal-safari/)
-* [Changelog](https://snappsu.github.io/Edemkal/edemkal-safari/changelog)
